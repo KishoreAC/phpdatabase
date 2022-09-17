e9de4b1389b78a7c88012682920b31e484deb4b7
KishoreAC <80164057+KishoreAC@users.noreply.github.com>
2022-09-17 22:43:44 +0530

    edited connect string

--- index.php
+++ index.php
@@ -6,13 +6,13 @@ echo "<br>To test the database, hit the dbtest.php URL";
 
 echo "<br><br>This assumes that you have the correct env variables set";
 echo "<BR><BR>The environment variables required are databaseuser, databasepassword, and databasenmae.";
-$dbhost = getenv("127.8.0.1");
+$dbhost = getenv("127.8.0.1:3306");
 $dbport = getenv("3306");
 $dbuser = getenv("chandana");
 $dbpwd = getenv("parents");
 $dbname = getenv("MaRa-I-ChaGe-CountEntry-DB");
 
-$connection = new mysqli($dbhost, $dbuser, $dbpwd, $dbname);
+$mysqli = mysqli_connect($dbhost, $dbuser, $dbpwd, $dbname); 
 if ($connection->connect_errno) {
     printf("Connect failed: %s\n", $mysqli->connect_error);
     exit();
