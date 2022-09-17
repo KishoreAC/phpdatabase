d72c36a52e922d7792c060d08e63d93bb4d97af8
KishoreAC <80164057+KishoreAC@users.noreply.github.com>
2022-09-17 22:16:15 +0530

    updates with connection variables
    $dbhost = getenv("172.18.0.1"); $dbport = getenv("3306"); $dbuser = getenv("chandana"); $dbpwd = getenv("parents"); $dbname = getenv("MaRa-I-ChaGe-CountEntry-DB");  $connection = new mysqli($dbhost, $dbuser, $dbpwd, $dbname); if ($connection->connect_errno) {     printf("Connect failed: %s\n", $mysqli->connect_error);     exit(); } else {     printf("Connected to the database"); } $connection->close();

--- index.php
+++ index.php
@@ -7,3 +7,18 @@ echo "<br>To test the database, hit the dbtest.php URL";
 echo "<br><br>This assumes that you have the correct env variables set";
 echo "<BR><BR>The environment variables required are databaseuser, databasepassword, and databasenmae.";
 
+$dbhost = getenv("172.18.0.1");
+$dbport = getenv("3306");
+$dbuser = getenv("chandana");
+$dbpwd = getenv("parents");
+$dbname = getenv("MaRa-I-ChaGe-CountEntry-DB");
+
+$connection = new mysqli($dbhost, $dbuser, $dbpwd, $dbname);
+if ($connection->connect_errno) {
+    printf("Connect failed: %s\n", $mysqli->connect_error);
+    exit();
+} else {
+    printf("Connected to the database");
+}
+$connection->close();
+?>
