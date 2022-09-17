679180f213bf989307fc88b26fecc4e5402e18b0
KishoreAC <80164057+KishoreAC@users.noreply.github.com>
2022-09-17 22:06:20 +0530

    Updated with connection variables
    Updated with connection variables

--- dbtest.php
+++ dbtest.php
@@ -1,9 +1,9 @@
 <?php
-$dbhost = getenv("MYSQL_SERVICE_HOST");
-$dbport = getenv("MYSQL_SERVICE_PORT");
-$dbuser = getenv("databaseuser");
-$dbpwd = getenv("databasepassword");
-$dbname = getenv("databasename");
+$dbhost = getenv("172.18.0.1");
+$dbport = getenv("3306");
+$dbuser = getenv("chandana");
+$dbpwd = getenv("parents");
+$dbname = getenv("MaRa-I-ChaGe-CountEntry-DB");
 
 $connection = new mysqli($dbhost, $dbuser, $dbpwd, $dbname);
 if ($connection->connect_errno) {
